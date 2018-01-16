a63bd8bc772c9e0ae6210be9388fe57c7b05a2a2
Minanshu Singh <minanshu.1998@gmail.com>
2018-01-16 16:21:22 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,77 @@
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
+    
+    
+mongoose.connect("mongodb://localhost/nippon_journey", { useMongoClient: true });
+mongoose.Promise = global.Promise;
+
+var nipponSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    desc: String
+});
+
+var Tourplaces = mongoose.model("Tourplaces",nipponSchema); // we created a model named Tourplaces
+
+/*Tourplaces.create({"name" : "Golden Pavilion", "image": "http://cdn.touropia.com/gfx/d/famous-temples/golden_pavilion.jpg?v=9ab0d0d9d29b808ee01a6e488a89500e",
+                    "desc" : "This is a palace build during the 1900s by the royal emperors of Japan in favour to Buddha."}
+                   , function(err, place) {
+        if(err) console.log("Error");
+        else {
+            console.log("Saved: ");
+            console.log(place);
+        }
+    });
+*/
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+   res.render("LandingPage"); 
+});
+app.get("/places",function(req, res) {
+    //get all tourplaces from database
+    Tourplaces.find({}, function(err, landmarks) {
+        if(err) console.log("EROROROR!");
+        else {
+            res.render("places",{landmarks:landmarks});      
+        }
+    });
+});
+app.get("/places/new", function(req, res) {
+   res.render("newPlace"); 
+});
+app.post("/places", function(req, res) { 
+    
+    var name = req.body.name;
+    var image = req.body.image;
+    var desc = req.body.desc;
+    var newPlace = {"name" : name, "image" : image, "desc": desc};
+    //create a new tourplace and save to databse
+    Tourplaces.create(newPlace, function(err, place) {
+       if (err) console.log("Eror");
+       else {
+           console.log("Saved!");
+           console.log(place);
+           res.redirect("/places");
+       }
+    });
+});
+
+//SHOW - shows more info about the tour place
+app.get("/places/:id", function(req, res) {
+   //get the description of the place with the id
+   Tourplaces.findById(req.params.id, function(err, foundPlace) {
+      if(err) console.log("Error in show description!");
+      else {
+        //render the show page for the place
+        res.render("show",{place:foundPlace});       
+      }
+   });
+   
+});
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("The Nippon server has started!");
+});
