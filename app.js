d53ac0096ae0c5d6e555c45dd7b6440dce940de0
Minanshu Singh <minanshu.1998@gmail.com>
2018-05-27 00:31:14 +0000

    add new comment feature enabled

--- app.js
+++ app.js
@@ -25,30 +25,27 @@ seedDB(); //seed the database with initial data
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", function(req, res) {
+app.get("/", (req, res) =>{
     console.log("Landing page called!!");    
     res.render("LandingPage"); 
 });
-app.get("/places",function(req, res) {
+app.get("/places",(req, res) =>{
     //get all tourplaces from database
-    Tourplaces.find({}, function(err, landmarks) {
+    Tourplaces.find({}, (err, landmarks) =>{
         if(err) console.log("EROROROR!");
         else {
-            res.render("places",{landmarks:landmarks});      
+            res.render("tourPlaces/index",{landmarks:landmarks});      
         }
     });
 });
-app.get("/places/new", function(req, res) {
-   res.render("newPlace"); 
+app.get("/places/new", (req, res) =>{
+   res.render("tourPlaces/new"); 
 });
-app.post("/places", function(req, res) { 
-    
-    var name = req.body.name;
-    var image = req.body.image;
-    var desc = req.body.desc;
-    var newPlace = {"name" : name, "image" : image, "desc": desc};
+
+app.post("/places", (req, res) =>{
+    var newPlace = req.body.place;
     //create a new tourplace and save to databse
-    Tourplaces.create(newPlace, function(err, place) {
+    Tourplaces.create(newPlace, (err, place) =>{
        if (err) console.log("Eror");
        else {
            console.log("Saved!");
@@ -59,18 +56,47 @@ app.post("/places", function(req, res) {
 });
 
 //SHOW - shows more info about the tour place
-app.get("/places/:id", function(req, res) {
+app.get("/places/:id", (req, res) =>{
    //get the description of the place with the id
    Tourplaces.findById(req.params.id).populate('comments').exec((err, foundPlace) => {
       if(err) console.log("Error in show description!");
       else {
         //render the show page for the place
-        res.render("show",{place:foundPlace});       
+        res.render("tourPlaces/show",{place:foundPlace});       
         console.log(foundPlace);
       }
    });
    
 });
+
+//COMMENTS routes
+app.get('/places/:id/comments/new', (req, res) => {
+    Tourplaces.findById(req.params.id, (err, foundPlace) => {
+       if(err) console.log(err);
+       else {
+           res.render('comments/new', {place: foundPlace});
+       }
+    });
+});
+app.post('/places/:id/comments', (req, res) => {
+   Tourplaces.findById(req.params.id, (err, foundPlace) => {
+      if(err){
+          console.log(err);
+          res.redirect('/places/'+req.params.id);
+      } 
+      else {
+          Comment.create(req.body.comment, (err, comment) => {
+             if(err) console.log(err);
+             else {
+                foundPlace.comments.push(comment);
+                foundPlace.save();
+                res.redirect('/places/'+req.params.id);
+             }
+          });
+      }
+   });
+});
+
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("The Nippon server has started!");
    // console.log(process.env.IP);
